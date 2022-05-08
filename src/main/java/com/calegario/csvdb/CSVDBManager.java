d4d0a3ae0a721f7b0e76bce02d482767d165e12d
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-05-08 15:07:05 -0300

    Fixed exceptions

--- src/main/java/com/calegario/csvdb/CSVDBManager.java
+++ src/main/java/com/calegario/csvdb/CSVDBManager.java
@@ -1,18 +1,12 @@
 package com.calegario.csvdb;
 
 import com.opencsv.*;
-import java.io.IOException;
-import java.io.Writer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.*;
+import java.lang.*;
 import java.util.ArrayList;
+import java.nio.file.Files;
 import java.util.List;
-import java.io.FileReader;
-import javax.swing.JOptionPane;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.lang.ClassCastException;
-import java.lang.ArrayIndexOutOfBoundsException;
+import java.nio.file.Paths;
 
 public class CSVDBManager{
     private String csvPath;
@@ -23,14 +17,16 @@ public class CSVDBManager{
         this.header = header;
     }
 
-    public void addRow(String[] row) {
+    public void addRow(String[] row) throws FileNotFoundException, IOException {
         List<String[]> data = getDB();
         data.remove(0);
         data.add(row);
         newDB(data);
     }
 
-    public void removeRow(String toFindValue, int column) {
+    public void removeRow(String toFindValue, int column)
+        throws FileNotFoundException, IOException
+    {
         List<String[]> data = getDB();
         data.remove(0);
         int removedCounter = 0;
@@ -42,17 +38,16 @@ public class CSVDBManager{
         }
         if (removedCounter != 0) {
             newDB(data);
-            JOptionPane.showMessageDialog(
-                null, String.valueOf(removedCounter) + " row(s) removed!"
-            );
         } else {
-            JOptionPane.showMessageDialog(
-                null, "No '" + toFindValue + "' found"
+            throw new ArrayIndexOutOfBoundsException(
+                "No '" + toFindValue + "' found"
             );
         }
     }
 
-    public void removeRow(String toFindValue, String columnName) {
+    public void removeRow(String toFindValue, String columnName)
+        throws FileNotFoundException, IOException
+    {
         int column = getColumnIndex(columnName);
         removeRow(toFindValue, column);
     }
@@ -66,69 +61,37 @@ public class CSVDBManager{
         throw new ArrayIndexOutOfBoundsException("Column name not found!");
     }
 
-    public List<String[]> getDB(){
+    public List<String[]> getDB() throws FileNotFoundException, IOException{
+        /**
+         * Returns a table with the data of the CSV file
+        **/
         List<String[]> data = new ArrayList<String[]>();
-
-        try {
-            CSVReader reader = new CSVReader(new FileReader(csvPath));
-            String[] nextLine;
-            while ((nextLine = reader.readNext()) != null) {
-                data.add(nextLine);
-            }
-            return data;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            data.add(new String[]{"Empty"});
-        } catch (ClassCastException e) {
-            e.printStackTrace();
+        CSVReader reader = new CSVReader(new FileReader(csvPath));
+        String[] nextLine;
+        while ((nextLine = reader.readNext()) != null) {
+            data.add(nextLine);
         }
         return data;
     }
 
-    public void newDB(List<String[]> data){
-        try {
-            Writer writer = Files.newBufferedWriter(Paths.get(csvPath));
-            CSVWriter csvWriter = new CSVWriter(writer);
-            csvWriter.writeNext(header);
-            csvWriter.writeAll(data);
-            csvWriter.flush();
-            csvWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void newDB(List<String[]> data)
+        throws FileNotFoundException, IOException
+    {
+        Writer writer = Files.newBufferedWriter(Paths.get(csvPath));
+        CSVWriter csvWriter = new CSVWriter(writer);
+        csvWriter.writeNext(header);
+        csvWriter.writeAll(data);
+        csvWriter.flush();
+        csvWriter.close();
     }
 
-    public void printDB(){
+    public void printDB() throws FileNotFoundException, IOException {
         List<String[]> savedData = getDB();
         for (int i = 0; i < savedData.size(); i++) {
           System.out.println(savedData.get(i)[0] + "; " +
                              savedData.get(i)[1] + "; " +
                              savedData.get(i)[2]);
         }
-    }
-
-    public String findValue(String columnsToLook, String valueToLook,
-                            String columnToRetrive){
-        /**
-         * Retrieves the rows where the passed value to be looked
-         for are present on the value of the passed column
-        **/
-        List<String[]> data = new ArrayList<String[]>();
-        int indexToLook = getColumnIndex(columnsToLook);
-        int indexToRetrieve = getColumnIndex(columnToRetrive);
-        data = getDB();
-        for (int i = 0; i < data.size(); i++) {
-            String currValue = data.get(i)[indexToLook];
-            if (currValue.contains(valueToLook)) {
-                return data.get(i)[indexToRetrieve];
-            }
-        }
-        return "";
     }
 
 }
